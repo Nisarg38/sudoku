2c14de7ae812f9a9dbb8bc7ead61b5ea35eb7b17
Nisarg38 <nisarg38@gmail.com>
2021-01-26 21:45:59 +0530

    App commit

---
+++ Sudoku-App/back-end/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nisarg/Development/sudoku-game/Sudoku-App/back-end/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
